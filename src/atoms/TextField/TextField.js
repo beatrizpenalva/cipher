b712ec0b13970b819cc9c44995bff9af6cca7b3e
Beatriz Penalva <biapenalva@gmail.com>
2021-10-10 13:11:15 -0300

    style: text field

--- src/atoms/TextField/TextField.js
+++ src/atoms/TextField/TextField.js
@@ -5,17 +5,18 @@ const TextField = ({ label, placeholder }) => {
   const handleChange = (e) => {
     console.log(e.target.value);
   };
-  
+
   return (
     <>
-      <label htmlFor="offset">
+      <label htmlFor="offset" className="input-sm-label">
         {label}
         <input
+          className="text-field"
           id="offset"
           type="number"
           placeholder={placeholder}
-          maxLength="1"
-          minLength="999"
+          maxLength="999"
+          minLength="1"
           onChange={(e) => handleChange(e)}
         ></input>
       </label>
