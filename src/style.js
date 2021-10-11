8fa5c3aad6a8703dc770d0f864a09ed75796335f
Beatriz Penalva <biapenalva@gmail.com>
2021-10-11 11:57:16 -0300

    chore: change css variants to js variants

---
+++ src/style.js
@@ -0,0 +1,13 @@
+export const tokens = {
+  primaryColor: "#6a63fe",
+  secondaryColor: "#3700bc",
+  grayDarker: "#212121",
+  grayLighter: "#FAFAFA",
+  black_10: "rgba(0, 0, 0, 0.1)",
+  h1: "normal 2.8rem",
+  h2: "normal 1.5rem",
+  p: "lighter 1.25rem",
+  pSamll: "lighter 1rem",
+  btn: "normal 1rem",
+  transitionHover: "all 0.5s ease-out",
+};
