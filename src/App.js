9c5fd88429d279fd15b8dd95b6a105c06fc5f660
Beatriz Penalva <biapenalva@gmail.com>
2021-10-10 21:46:45 -0300

    feat: create and set states

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "./atoms/Button";
 import BoxMessage from "./atoms/BoxMessage";
 import Counter from "./atoms/Counter";
 import RadioButton from "./atoms/RadioButton";
-import TextField from "./atoms/TextField";
+import InputNumber from "./atoms/InputNumber";
+import { encrypt } from "./utils/cipher";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messageEncrypted, setMessageEncrypted] = useState("");
+  const [offset, setOffset] = useState();
+  const [method, setMethod] = useState("");
+  //const [caracteresNumber, setCaracteresNumber] = useState();
+
+  useEffect(() => {
+    if (method && offset && message) {
+      const finalMessage = encrypt(offset, message, method);
+      setMessageEncrypted(finalMessage);
+    }
+  }, [method, offset, message]);
+
+  const setOffState = (offsetNumber) => {
+    setOffset(offsetNumber);
+  };
+
+  const setMessageState = (text) => {
+    setMessage(text);
+  };
+
+  const setMethodState = (methodToApply) => {
+    setMethod(methodToApply);
+  };
+
   return (
     <div className="App">
-      <TextField label={"Key"} placeholder={"1-999"} />
+      <InputNumber
+        label={"Key"}
+        placeholder={"1-999"}
+        setOffState={setOffState}
+      />
       <Counter value={0} maxValue={2000} />
       <BoxMessage
         label={"Input"}
         placeholder={"Type your message here"}
         spellCheck={true}
         readOnly={false}
+        action={setMessageState}
       />
-      <RadioButton method={"decode"} />
-      <RadioButton method={"encode"} />
+      <RadioButton method={"decode"} action={setMethodState} />
+      <RadioButton method={"encode"} action={setMethodState} />
 
       <BoxMessage
         label={"Output"}
-        placeholder={"Message encrypted"}
+        placeholder={"The message encrypted will appear here."}
         spellCheck={false}
         readOnly={true}
+        value={messageEncrypted}
       />
       <Button action={"copy"} />
       <Button action={"clear"} />
