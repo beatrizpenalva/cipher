7f50c9f207d78cabdcec7a9b512c06de87532c53
Beatriz Penalva <biapenalva@gmail.com>
2021-10-11 12:21:52 -0300

    chore: add font family

--- src/style.js
+++ src/style.js
@@ -1,13 +1,15 @@
+const fontBody = "Oswald";
+
 export const tokens = {
   primaryColor: "#6a63fe",
   secondaryColor: "#3700bc",
   grayDarker: "#212121",
   grayLighter: "#FAFAFA",
   black_10: "rgba(0, 0, 0, 0.1)",
-  h1: "normal 2.8rem",
-  h2: "normal 1.5rem",
-  p: "lighter 1.25rem",
-  pSamll: "lighter 1rem",
-  btn: "normal 1rem",
+  h1: `normal 2.8rem ${fontBody}, sans-serif`,
+  h2: `normal 1.5rem ${fontBody}, sans-serif`,
+  p: `lighter 1.25rem ${fontBody}, sans-serif`,
+  pSamll: `lighter 1rem ${fontBody}, sans-serif`,
+  btn: `normal 1rem ${fontBody}, sans-serif`,
   transitionHover: "all 0.5s ease-out",
 };
