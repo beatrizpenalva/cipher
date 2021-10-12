633b060a84863e81474d591422ed4a51ee172fe6
Beatriz Penalva <biapenalva@gmail.com>
2021-10-12 06:50:33 -0300

    chore: add logo typography

--- src/style.js
+++ src/style.js
@@ -1,3 +1,4 @@
+const fontLogo = "Staatliches";
 const fontBody = "Oswald";
 
 export const tokens = {
@@ -7,7 +8,7 @@ export const tokens = {
   grayLight: "#bdbdbd",
   grayLighter: "#FAFAFA",
   black_10: "rgba(0, 0, 0, 0.1)",
-  h1: `normal 2.8rem ${fontBody}, sans-serif`,
+  h1: `normal 2.8rem ${fontLogo}, sans-serif`,
   h2: `normal 1.5rem ${fontBody}, sans-serif`,
   p: `lighter 1.25rem ${fontBody}, sans-serif`,
   pSamll: `lighter 1rem ${fontBody}, sans-serif`,
